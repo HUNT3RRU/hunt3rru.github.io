588cab5ebe84237ea2a625e7c72071570d6eef2b
HUNT3RRU <839709@pdsb.net>
2025-01-14 19:33:59 -0500

    Add files via upload

---
+++ projects/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    // Your code here
+    function initialize() {
+        // Initialization code
+    }
+
+    initialize();
+});
